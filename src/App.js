5414a13a4c48188bb5eb57b19083b423182b97e3
Pham Phuong Nam <nampp@infoplusvn.com>
2020-10-29 10:50:38 +0700

    up project demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Admin, Resource, fetchUtils } from 'react-admin';
+import {objCreate, objEdit, objList} from "./Demo";
+import { stringify } from "query-string";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const apiUrl = "http://localhost:8080";
+const httpClient = fetchUtils.fetchJson;
+
+const  dataProvider= {
+    getList: (resource, params) => {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify(params.filter)
+        };
+        const url = `${apiUrl}/${resource}?${stringify(query)}`;
+        return httpClient(url).then(({ headers, json }) => ({
+            data: json,
+            total: 10
+        }));
+    },
+    getOne: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}/${params.id}`).then(({ json }) => ({
+            data: json,
+        })),
+
+    getMany: (resource, params) => {
+        const query = {
+            filter: JSON.stringify({ id: params.ids }),
+        };
+        const url = `${apiUrl}/${resource}?${stringify(query)}`;
+        return httpClient(url).then(({ json }) => ({ data: json }));
+    },
+
+    getManyReference: (resource, params) => {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify({
+                ...params.filter,
+                [params.target]: params.id,
+            }),
+        };
+        const url = `${apiUrl}/${resource}?${stringify(query)}`;
+
+        return httpClient(url).then(({ headers, json }) => ({
+            data: json,
+            total: parseInt(headers.get('content-range').split('/').pop(), 10),
+        }));
+    },
+
+    update: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}/${params.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(params.data),
+        }).then(({ json }) => ({ data: json })),
+
+    updateMany: (resource, params) => {
+        const query = {
+            filter: JSON.stringify({ id: params.ids}),
+        };
+        return httpClient(`${apiUrl}/${resource}?${stringify(query)}`, {
+            method: 'PUT',
+            body: JSON.stringify(params.data),
+        }).then(({ json }) => ({ data: json }));
+    },
+
+    create: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}`, {
+            method: 'POST',
+            body: JSON.stringify(params.data),
+        }).then(({ headers,json }) => ({
+            data: { ...params.data },
+        })),
+
+    delete: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}/${params.id}`, {
+            method: 'DELETE',
+        }).then(({ json }) => ({ data: json })),
+
+    deleteMany: (resource, params) => {
+        const query = {
+            filter: JSON.stringify({ id: params.ids}),
+        };
+        return httpClient(`${apiUrl}/${resource}?${stringify(query)}`, {
+            method: 'DELETE',
+            body: JSON.stringify(params.data),
+        }).then(({ json }) => ({ data: json }));
+    }
+};
+class AdminPanel extends React.Component {
+    render() {
+        return (
+            <div>
+                <Admin dataProvider={dataProvider}>
+                    <Resource name = 'demo' list = {objList} edit={objEdit} create={objCreate} />
+                </Admin>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default AdminPanel;
+
+
+
---
+++ src/Demo.js
@@ -0,0 +1,32 @@
+import * as React from "react";
+import { List, Datagrid, Edit, Create, SimpleForm, DateField, TextField, EditButton, TextInput, DateInput } from 'react-admin';
+
+
+export const objList = props => (
+    <List {...props}>
+        <Datagrid>
+            <TextField source="id"/>
+            <TextField source="name"/>
+            <DateField source="dob" locales="fr-FR" />
+            <EditButton basePath="/Demo"/>
+        </Datagrid>
+    </List>
+);
+
+export const objEdit = (props) => (
+    <Edit{...props}>
+        <SimpleForm>
+            <TextInput source="name"/>
+            <DateInput source="dob" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const objCreate = (props) => (
+    <Create{...props}>
+        <SimpleForm>
+            <TextInput source="name"/>
+            <DateInput source="dob" />
+        </SimpleForm>
+    </Create>
+);
